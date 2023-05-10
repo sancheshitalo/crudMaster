799b458e9cc589caaa0f99d2dbc64c69103d0b09
sancheshitalo <sancheshitalo@gmail.com>
2023-05-09 23:03:13 -0300

    restAPI, HTTP, statusCode, verbos

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const app = express()
+const port = 3000;
+
+//MIDDLEWARE
+app.use(express.json());
+
+// ROTAS
+app.get('/', (req, res) => {
+    const data = req.body
+    const clienteSalvo = data
+    const dadosAdicionais = {
+        city: 'maringá'
+    }
+    resp.status(201).send({
+        ...clienteSalvo,
+        ...dadosAdicionais
+    })
+});
+
+app.get('/buscar-clientes', (req, resp) => {
+    const clientes = [
+        {name: 'weslei'},
+        {name: 'hitalo'},
+        {name: 'bigode'},
+    ]
+
+    const params = req.query
+    
+    if(!isObjectEmpty(params)){
+        return resp.status(400).send('Nenhum parâmetro de pesquisa informado')
+    }
+
+    const clienteEncontrado = clientes.filter((client) => client.name === params.name)
+
+    resp.json(clienteEncontrado)
+})
+
+app.post('/cadastrar-cliente', (req, resp) => {
+    resp.status(201).send('Cliente cadastrado')
+});
+
+//START SERVER
+app.listen(port, () => {
+    console.log('Servidor online na porta 3000.')
+});
+
+//UTILS
+const isObjectEmpty = (obj) => {
+    const hasKeys = Object.keys(obj)
+    return hasKeys.length > 0
+}
